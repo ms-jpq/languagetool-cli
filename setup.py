1208ce884a725843992b567735a2da3761ded96d
Fly me to the moon <git@bigly.dog>
2021-01-24 03:28:23 +0700

    added ci

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+packages = find_packages(exclude=("tests*",))
+package_data = {pkg: ("py.typed",) for pkg in packages}
+
+setup(
+    name="languagetool_cli",
+    python_requires=">=3.8.0",
+    version="0.1.0",
+    description="LanguageTool CLI",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="ms-jpq",
+    author_email="github@bigly.dog",
+    url="https://github.com/ms-jpq/languagetool_cli",
+    packages=packages,
+    package_data=package_data,
+)
